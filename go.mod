09da8b8a85bf66aa20035617ef26beed523cb75f
duyi <2727584152@qq.com>
2020-04-05 21:26:49 +0800

    Add: ConnDB SetDB GetDB in db.go

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module goweb
 
 go 1.13
+
+require (
+	github.com/garyburd/redigo v1.6.0
+	github.com/gin-gonic/gin v1.6.2
+	golang.org/x/tools v0.0.0-20180917221912-90fa682c2a6e
+)
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "goweb/db"
+import (
+	"goweb/db"
+	"goweb/utils"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	db.Test()
+	r := db.ConnDB("localhost:6379")
+	if r == nil {
+		utils.ColorPrintf("database is not ready\n", utils.Red)
+		os.Exit(1)
+	}
+	g := gin.Default()
+	g.GET("/set", func(c *gin.Context) {
+		key := c.Query("key")
+		value := c.Query("val")
+		reply := db.SetDB(r, key, value)
+		c.String(http.StatusOK, "%s\n", reply)
+	})
+	g.GET("/get", func(c *gin.Context) {
+		key := c.Query("key")
+		reply := db.GetDB(r, key)
+		c.String(http.StatusOK, "%s\n", reply)
+	})
+	g.Run("localhost:8080")
+
 }
